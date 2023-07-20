2413a2718dcb467f3caf262283fc241e2ae181aa
Brendan <btms977@gmail.com>
2023-07-20 11:53:47 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,7 +13,6 @@ p_l = []
 change= []
 
 
-
 with open(csv_path) as csvfile:
 
     csvreader= csv.reader(csvfile, delimiter=',')
@@ -23,14 +22,21 @@ with open(csv_path) as csvfile:
         months.append(row[0])
         #Net Total
         net_total.append(int(row[1]))
-        #Average Change
+        #Average Change(pt.1)
         p_l.append(int(row[1]))
     for a, b  in zip(p_l[0::], p_l[1::]):
+        #Average Change(pt.2)
         change.append(int(b-a))
         avg_change= np.mean(change)
+    #Greatest Increase
+    increase= max(change)
+    #Greatest Decrease
+    decrease = min(change)
     print(f'Total Months: {len(months)}')
     print(f'Total: ${sum(net_total)}')
     print(f'Average Change: ${round(avg_change, 2)}')
+    print(f'Greatest Increase in Profits: ${increase}')
+    print(f'Greatest Decrease in Profits: ${decrease}')
 
 
   
